2775c01e74a020119343a17bba85da9de73f2604
J Lee <ljinlabs@gmail.com>
2020-06-09 17:27:27 +0900

    Added templates and configured static files

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -5,8 +5,8 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title','slug','status','created_on')
-    list_filter = ("status")
+    list_filter = ("status",)
     search_fields = ['title','content']
     prepopulated_fields = {'slug':('title',)}
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
